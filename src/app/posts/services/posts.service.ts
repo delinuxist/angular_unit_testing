14983a23b2d95b532f7759d491b905304319043e
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-07-19 13:23:07 +0000

    post services

---
+++ src/app/posts/services/posts.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
+import { PostInterface } from '../types/type';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostsService {
+  getPosts(): Observable<PostInterface[]> {
+    const posts = [
+      { id: '1', title: 'First post' },
+      { id: '2', title: 'Second post' },
+      { id: '3', title: 'Third post' },
+    ];
+    return of(posts).pipe(delay(2000));
+  }
+}
